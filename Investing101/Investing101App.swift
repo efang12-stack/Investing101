035b8d96dfc4946e35d620751261ffa5a9a94337
Ethan Fang <ethanfang10@gmail.com>
2021-01-12 21:04:10 -0600

    Initial Commit

---
+++ Investing101/Investing101App.swift
@@ -0,0 +1,17 @@
+//
+//  Investing101App.swift
+//  Investing101
+//
+//  Created by Ethan on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Investing101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
