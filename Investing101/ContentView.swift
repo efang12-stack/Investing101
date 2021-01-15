a508220c4493bd04db64ccbff6ea63a7e464b9f3
Ethan Fang <ethanfang10@gmail.com>
2021-01-14 20:11:08 -0600

    just finished side bar but without navigating to new views

--- Investing101/ContentView.swift
+++ Investing101/ContentView.swift
@@ -10,47 +10,152 @@ import SwiftUI
 struct ContentView: View {
     @State var showMenu = false
     var body: some View {
-        
-        let drag = DragGesture()
-            .onEnded {
-                if $0.translation.width < -100 {
-                    withAnimation {
-                        self.showMenu = false
+        Home()
+}
+}
+
+struct Home: View {
+    @State var show = false
+    var body: some View{
+        ZStack(alignment: .leading) {
+            GeometryReader{_ in
+                VStack{
+                    
+                    ZStack{
+                        HStack{
+                            
+                            Button(action: {
+                                withAnimation(.default){
+                                    self.show.toggle()
+                                }
+                            }, label: {
+                                Image(systemName: "line.horizontal.3")
+                                    .resizable()
+                                    .frame(width: 25, height: 25)
+                            })
+                            
+                            Spacer()
+                        }
+                        
+                        Text("Courses")
+                            .bold()
                     }
+                    .padding()
+                    .foregroundColor(.primary)
+                    .overlay(Rectangle().stroke(Color.primary.opacity(0.1), lineWidth: 1).shadow(radius: 3).edgesIgnoringSafeArea(.top))
+                    
+                    Spacer()
+                    
+                   Text("Dark Mode Menu")
+                    
+                    Spacer()
                 }
             }
-        return NavigationView {
-            GeometryReader { geometry in
-                ZStack(alignment: .leading) {
-                    MainView()
-                        .frame(width: geometry.size.width, height: geometry.size.height)
-                        .offset(x: self.showMenu ? geometry.size.width/2 : 0)
-                        .disabled(self.showMenu ? true : false)
-                    if self.showMenu {
-                        MenuView()
-                            .frame(width: geometry.size.width/2)
-                            .transition(.move(edge: .leading))
-                    }
-                }
-                .gesture(drag)
+            
+            
+            HStack {
+                Menu(show: $show)
+                    .offset(x: self.show ? 0 : -UIScreen.main.bounds.width / 1.7)
             }
-            .navigationBarTitle("Side menu", displayMode: .inline)
-            .navigationBarItems(leading: (
-                Button(action: {
-                    self.showMenu.toggle()
-                }, label: {
-                    Image(systemName: "line.horizontal.3")
-                        .imageScale(.large)
-                })
-            ))
+                    
+                
+            
+            
         }
     }
 }
 
-struct MainView: View {
+struct Menu: View {
+    
+    @Binding var show: Bool
     
     var body: some View{
-        Text("Hi")
+        VStack{
+            
+            HStack{
+                Button(action: {
+                    withAnimation(.default){
+                        self.show.toggle()
+                    }
+                }, label: {
+                    Image(systemName: "chevron.backward")
+                        .resizable()
+                        .frame(width: 12, height: 20)
+                })
+                
+                Spacer()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "square.and.pencil")
+                        .font(.title)
+                })
+                
+            }
+            .padding(.top, 50)
+            .padding(.bottom, 25)
+            
+            Group {
+                Button(action: {
+                    
+                }, label: {
+                    HStack(spacing: 22){
+                        Image(systemName: "house")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(.black)
+                        
+                        Text("Home")
+                        
+                        
+                    }
+                    .padding(.trailing, 50)
+                })
+                .padding(.top, 25)
+                
+                Button(action: {
+                    
+                }, label: {
+                    HStack(spacing: 22){
+                        Image(systemName: "newspaper")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(.black)
+                        
+                        Text("Articles")
+                        
+                        
+                    }
+                    .padding(.trailing, 40)
+                })
+                .padding(.top, 25)
+                
+                Button(action: {
+                    
+                }, label: {
+                    HStack(spacing: 18){
+                        Image(systemName: "phone")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(.black)
+                        
+                        Text("Contact Us")
+                            
+                            
+                    }
+                    .padding(.trailing, 15)
+                })
+                .padding(.top, 25)
+            }
+            
+            Spacer()
+            
+        }
+        .foregroundColor(.primary)
+        .padding(.horizontal, 20)
+        .frame(width: UIScreen.main.bounds.width / 1.7)
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.all)
+        .overlay(Rectangle().stroke(Color.primary.opacity(0.2), lineWidth: 2).shadow(radius: 3).edgesIgnoringSafeArea(.all))
     }
 }
 
